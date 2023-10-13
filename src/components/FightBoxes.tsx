47c244214e248bc94401523ede2873c98247710e
Vollmer <kristianvollmer@gmail.com>
2023-10-13 13:35:57 +0200

    No more hidden input box

--- src/components/FightBoxes.tsx
+++ src/components/FightBoxes.tsx
@@ -3,27 +3,22 @@ import { ReportFight } from "../wcl/gql/types";
 
 export const FightBoxes = ({
   fights,
+  selectedIds,
   setSelectedIds,
 }: {
   fights: ReportFight[] | undefined;
   selectedIds: number[];
   setSelectedIds: (ids: number[]) => void;
 }) => {
-  const handleCheckboxChange = (
-    e: React.ChangeEvent<HTMLInputElement>,
-    id: number
-  ) => {
-    const isChecked = e.target.checked;
-
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    setSelectedIds((prevSelectedIds: number[]) => {
-      if (isChecked) {
-        return [...prevSelectedIds, id];
-      } else {
-        return prevSelectedIds.filter((selectedId) => selectedId !== id);
-      }
-    });
+  const handleDivClick = (id: number) => {
+    const isSelected = selectedIds.includes(id);
+    if (isSelected) {
+      // Deselect the fight
+      setSelectedIds(selectedIds.filter((selectedId) => selectedId !== id));
+    } else {
+      // Select the fight
+      setSelectedIds([...selectedIds, id]);
+    }
   };
 
   if (!fights) {
@@ -35,17 +30,15 @@ export const FightBoxes = ({
       {fights
         .filter((fight) => fight.difficulty)
         .map((fight) => (
-          <div key={fight.id} className="checkbox-fight">
-            <input
-              type="checkbox"
-              id={fight.id.toString()}
-              value={fight.id}
-              onChange={(e) => handleCheckboxChange(e, fight.id)}
-            />
-            <label
-              htmlFor={fight.id.toString()}
-              id={fight.kill ? "fight-kill" : "fight-wipe"}
-            >
+          <div
+            key={fight.id}
+            className={`checkbox-fight ${
+              selectedIds.includes(fight.id) ? "selected" : ""
+            }`}
+            id={fight.kill ? "fight-kill" : "fight-wipe"}
+            onClick={() => handleDivClick(fight.id)}
+          >
+            <label>
               {fight.name}
               <br />
               <span className="flavor-text">
