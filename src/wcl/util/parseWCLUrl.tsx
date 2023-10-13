1ab35a82ed27f8753c9bc6e5e8d9876c8c9ba17a
Vollmer <kristianvollmer@gmail.com>
2023-10-13 13:11:33 +0200

    Borrow Xephs WCL url parsing logic

--- src/wcl/util/parseWCLUrl.tsx
+++ src/wcl/util/parseWCLUrl.tsx
@@ -1,11 +1,53 @@
-export function parseWCLUrl(input: string) {
-  if (input.trim() === "") {
-    return;
+export const parseWCLUrl = (maybeURL: string) => {
+  if (isValidReportId(maybeURL)) {
+    return maybeURL;
   }
 
-  const url = new URL(input);
+  try {
+    const { pathname, host, hash } = new URL(maybeURL);
 
-  const reportCode = url.pathname.split("/").pop() ?? "";
+    // not a WCL url
+    if (
+      !host.includes("warcraftlogs.com") ||
+      !pathname.startsWith("/reports/")
+    ) {
+      return;
+    }
 
-  return reportCode;
-}
+    const maybeReportID = pathname.replace("/reports/", "").replace("/", "");
+
+    // WCL url, but doesnt point to reports
+    if (!isValidReportId(maybeReportID)) {
+      return;
+    }
+
+    // WCL url, points to reports, but no fight selected
+    if (!hash) {
+      return maybeReportID;
+    }
+
+    const maybeFightID = new URLSearchParams(hash.slice(1)).get("fight");
+
+    // no fightID at all
+    if (!maybeFightID) {
+      return maybeReportID;
+    }
+
+    // fightID may be `last` or numeric
+    if (maybeFightID === "last" || Number.parseInt(maybeFightID) > 0) {
+      return maybeReportID;
+    }
+
+    return maybeReportID;
+  } catch {
+    return;
+  }
+};
+
+const isValidReportId = (id?: string | string[]): id is string => {
+  if (!id || Array.isArray(id) || id.includes(".")) {
+    return false;
+  }
+
+  return (id.startsWith("a:") && id.length === 18) || id.length === 16;
+};
