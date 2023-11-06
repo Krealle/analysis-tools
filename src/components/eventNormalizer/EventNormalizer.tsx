174915284075cbd75c57ce5474de6d69d0889b88
Vollmer <kristianvollmer@gmail.com>
2023-11-06 18:51:39 +0100

    re-implement intervals

--- src/components/eventNormalizer/EventNormalizer.tsx
+++ src/components/eventNormalizer/EventNormalizer.tsx
@@ -4,19 +4,34 @@ import { Fight, generateFights } from "./generateFights";
 import FightButtons from "../FightButtons";
 import bearDancing from "/static/bear/dance.gif";
 import tableRenderer from "./tableRenderer";
+import { FormattedTimeSkipIntervals } from "../../helpers/types";
+import { formatTime } from "../../util/format";
+import { getAverageIntervals } from "./interval/intervals";
+import intervalRenderer from "./interval/intervalRenderer";
 let fights: Fight[] = [];
 const EventNormalizer = () => {
-  const [content, setContent] = useState<JSX.Element | null>(null);
+  const [wclTableContent, setWclTableContent] = useState<JSX.Element | null>(
+    null
+  );
+  const [intervalsContent, setIntervalsContent] = useState<JSX.Element | null>(
+    null
+  );
   const [isFetching, setIsFetching] = useState<boolean>(false);
 
   const WCLReport = useAppSelector((state) => state.WCLUrlInput.fightReport);
   const selectedFights = useAppSelector(
     (state) => state.fightBoxes.selectedIds
   );
-
-  const { parameterError, parameterErrorMsg } = useAppSelector(
-    (state) => state.customFightParameters
-  );
+  const {
+    parameterError,
+    parameterErrorMsg,
+    timeSkipIntervals,
+    abilityNoEMScaling,
+    abilityNoBoEScaling,
+    abilityNoScaling,
+    abilityBlacklist,
+    enemyBlacklist,
+  } = useAppSelector((state) => state.customFightParameters);
 
   const handleButtonClick = async () => {
     if (selectedFights.length === 0) {
@@ -29,18 +44,19 @@ const EventNormalizer = () => {
     }
 
     setIsFetching(true);
-    setContent(
+    setWclTableContent(
       <>
         <big>Fetching data</big>
         <br />
         <img src={bearDancing} />
       </>
     );
+    setIntervalsContent(null);
 
     try {
       await attemptNormalize();
     } catch (error) {
-      setContent(<>{error}</>);
+      setWclTableContent(<>{error}</>);
     }
 
     setIsFetching(false);
@@ -63,11 +79,33 @@ const EventNormalizer = () => {
         selectedFights.includes(fight.fightId)
       );
 
-      const content = tableRenderer(fightsToRender);
+      const wclTableContent = tableRenderer(fightsToRender);
 
-      setContent(content);
+      const formattedTimeSkipIntervals: FormattedTimeSkipIntervals[] = [];
+      for (const interval of timeSkipIntervals) {
+        const formattedStartTime = formatTime(interval.start);
+        const formattedEndTime = formatTime(interval.end);
+        if (formattedStartTime && formattedEndTime) {
+          formattedTimeSkipIntervals.push({
+            start: formattedStartTime,
+            end: formattedEndTime,
+          });
+        }
+      }
+
+      const intervals = getAverageIntervals(
+        fights,
+        selectedFights,
+        WCLReport.code,
+        formattedTimeSkipIntervals
+      );
+
+      const intervalContent = intervalRenderer(intervals, fights[0].combatants);
+
+      setWclTableContent(wclTableContent);
+      setIntervalsContent(intervalContent);
     } catch (error) {
-      setContent(<>{error}</>);
+      setWclTableContent(<>{error}</>);
     }
 
     setIsFetching(false);
@@ -79,7 +117,8 @@ const EventNormalizer = () => {
         isFetching={isFetching}
         handleButtonClick={handleButtonClick}
       />
-      {content}
+      {wclTableContent}
+      {intervalsContent}
     </div>
   );
 };
---
+++ src/components/eventNormalizer/interval/intervals.ts
@@ -0,0 +1,245 @@
+import {
+  FormattedTimeSkipIntervals,
+  IntervalEntry,
+  IntervalSet,
+  TotInterval,
+} from "../../../helpers/types";
+import { mrtColorMap } from "../../../util/constants";
+import { formatDuration } from "../../../util/format";
+import { Combatant } from "../combatant/combatants";
+import { Fight } from "../generateFights";
+
+export function getAverageIntervals(
+  fights: Fight[],
+  selectedFights: number[],
+  currentReportCode: string,
+  timeSkipIntervals: FormattedTimeSkipIntervals[]
+  /* abilityNoEMScaling: number[],
+  abilityNoBoEScaling: number[],
+  abilityNoScaling: number[],
+  abilityBrokenAttribution: number[] */
+): TotInterval[] {
+  const sortedIntervals: TotInterval[] = [];
+  console.log(fights);
+
+  for (const fight of fights) {
+    if (
+      !selectedFights.includes(fight.fightId) ||
+      fight.reportCode !== currentReportCode
+    ) {
+      continue;
+    }
+    let currentInterval = 1;
+
+    // TODO: should be variable
+    const intervalDur = 30_000;
+    let intervalTimer = fight.startTime;
+    let interval: IntervalSet = [];
+    let latestTimestamp = 0;
+
+    for (const event of fight.normalizedDamageEvents) {
+      if (
+        event.source.spec === "Augmentation" ||
+        event.normalizedAmount === 0
+      ) {
+        continue;
+      }
+
+      const overlapsWithTimeSkip = timeSkipIntervals.some((skipInterval) => {
+        return (
+          event.timestamp >= skipInterval.start + fight.startTime &&
+          event.timestamp <= skipInterval.end + fight.startTime
+        );
+      });
+
+      if (
+        event.timestamp > intervalTimer + intervalDur ||
+        overlapsWithTimeSkip
+      ) {
+        if (interval.length === 0 && overlapsWithTimeSkip) {
+          intervalTimer = event.timestamp;
+          continue;
+        }
+        const sortedInterval = interval.slice();
+        sortedInterval.sort((a, b) => b.damage - a.damage);
+
+        const existingEntry = sortedIntervals.find(
+          (entry) => entry.currentInterval === currentInterval
+        );
+
+        const endTimestamp =
+          latestTimestamp > 0 ? latestTimestamp : event.timestamp;
+        if (existingEntry) {
+          existingEntry.intervalEntries.push(sortedInterval);
+        } else {
+          sortedIntervals.push({
+            currentInterval,
+            intervalEntries: [sortedInterval],
+            start: intervalTimer - fight.startTime,
+            end: endTimestamp - fight.startTime,
+          });
+        }
+
+        intervalTimer = event.timestamp;
+        interval = [];
+        currentInterval++;
+        if (overlapsWithTimeSkip) {
+          continue;
+        }
+      }
+      latestTimestamp = event.timestamp;
+
+      const intervalEntry = interval.find(
+        (entry) => entry.id === event.source.id
+      );
+
+      const amount = event.normalizedAmount;
+
+      if (intervalEntry) {
+        intervalEntry.damage += amount;
+      } else {
+        interval.push({
+          id: event.source.id,
+          damage: amount,
+        });
+      }
+    }
+  }
+  return sortedIntervals;
+}
+
+export function averageOutIntervals(
+  totIntervals: TotInterval[]
+): TotInterval[] {
+  const avgIntervals: TotInterval[] = [];
+
+  for (const entry of totIntervals) {
+    const currentInterval = entry.currentInterval;
+    const intervalRecord: Record<
+      number,
+      { totalDamage: number; count: number }
+    > = {};
+
+    for (const interval of entry.intervalEntries) {
+      for (const { id, damage } of interval) {
+        if (!intervalRecord[id]) {
+          intervalRecord[id] = { totalDamage: damage, count: 1 };
+        } else {
+          intervalRecord[id].totalDamage += damage;
+          intervalRecord[id].count++;
+        }
+      }
+    }
+
+    const intervalSet: IntervalEntry[] = Object.entries(intervalRecord).map(
+      ([id, intervalData]) => ({
+        id: +id,
+        damage: intervalData.totalDamage / intervalData.count,
+      })
+    );
+
+    const sortedIntervalSet = intervalSet.sort((a, b) => b.damage - a.damage);
+
+    avgIntervals.push({
+      currentInterval: currentInterval,
+      intervalEntries: [sortedIntervalSet],
+      start: entry.start,
+      end: entry.end,
+    });
+  }
+
+  return avgIntervals;
+}
+
+export function getTop4Pumpers(topPumpersData: TotInterval[]): TotInterval[] {
+  return topPumpersData.map((interval) => ({
+    currentInterval: interval.currentInterval,
+    intervalEntries: [interval.intervalEntries[0].slice(0, 4)],
+    start: interval.start,
+    end: interval.end,
+  }));
+}
+
+export function getMRTNote(
+  avgTopPumpersData: TotInterval[],
+  combatants: Combatant[]
+): string {
+  const threshold: number = 1.5;
+  const defaultTargets: Set<number> = getDefaultTargets(avgTopPumpersData);
+
+  let note: string = "prescGlowsStart \n" + "defaultTargets - ";
+  note += [...defaultTargets]
+    .map((id) => {
+      const player = combatants.find((player) => player.id === id);
+
+      return mrtColorMap.get(player?.class ?? "") + (player?.name ?? "") + "|r";
+    })
+    .join(" ");
+  note += "\n";
+
+  avgTopPumpersData.forEach((interval, index) => {
+    const dataSet: IntervalSet = interval.intervalEntries[0];
+    const top2: Set<number> = new Set(
+      dataSet.slice(0, 2).map((entry) => entry.id)
+    );
+
+    let isImportant: boolean = false;
+    let top2Damage: number = 0;
+    let defaultDamage: number = 0;
+
+    for (const player of dataSet) {
+      if (defaultTargets.has(player.id)) {
+        defaultDamage += player.damage;
+      }
+      if (top2.has(player.id)) {
+        top2Damage += player.damage;
+      }
+    }
+
+    if (top2Damage > defaultDamage * threshold) {
+      isImportant = true;
+    }
+    if (index === 0) {
+      note += "PREPULL" + " - ";
+    } else {
+      note += formatDuration(interval.start) + " - ";
+    }
+
+    note += [...top2]
+      .map((id) => {
+        const player = combatants.find((player) => player.id === id);
+
+        return (
+          mrtColorMap.get(player?.class ?? "") + (player?.name ?? "") + "|r"
+        );
+      })
+      .join(" ");
+
+    note += `${isImportant ? " *" : ""} \n`;
+  });
+
+  note += "prescGlowsEnd";
+
+  return note;
+}
+
+function getDefaultTargets(avgTopPumpersData: TotInterval[]) {
+  const idSum = new Map<number, number>();
+
+  for (const interval of avgTopPumpersData) {
+    interval.intervalEntries[0].slice(0, 4).forEach((entry) => {
+      const currentSum = idSum.get(entry.id) ?? 0;
+      const sum = currentSum + entry.damage;
+
+      idSum.set(entry.id, sum);
+    });
+  }
+
+  const sortedIds = [...idSum.entries()]
+    .sort((a, b) => b[1] - a[1])
+    .slice(0, 2);
+
+  const defaultTargets: Set<number> = new Set(sortedIds.map(([id]) => id));
+
+  return defaultTargets;
+}
